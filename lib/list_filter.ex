5ae38fa6fbf02c39e962100d855c2bcffa5c4c77
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-10-04 19:27:12 +0200

    Implement algorithm

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,24 @@
 defmodule ListFilter do
   @moduledoc """
-  Documentation for `ListFilter`.
+  Filter odd numbers from a given string list and returns its quantity
   """
 
-  @doc """
-  Hello world.
+  import Integer, only: [is_odd: 1]
 
-  ## Examples
+  def call(list), do: list |> get_numbers_from_list |> get_odd_numbers_from_list |> length
 
-      iex> ListFilter.hello()
-      :world
+  defp get_numbers_from_list(string_list) do
+    string_list
+    |> Enum.map(fn element ->
+      case Integer.parse(element) do
+        {result, _remainder_of_binary} -> result
+        status -> status
+      end
+    end)
+    |> Enum.filter(fn element -> is_integer(element) end)
+  end
 
-  """
-  def hello do
-    :world
+  defp get_odd_numbers_from_list(integers_list) do
+    integers_list |> Enum.filter(fn element -> is_odd(element) end)
   end
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,7 +1,13 @@
 defmodule ListFilterTest do
   use ExUnit.Case
 
-  test "get quantity of even numbers from list" do
-    assert ListFilter.hello() == :world
+  test "get quantity of odd numbers from string list" do
+    list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+    response = ListFilter.call(list)
+
+    expected_response = 3
+
+    assert response == expected_response
   end
 end
